b5e0b083a128a5aaa8572ff7449e42c523dfa69f
Krista Jackson <krista.lynn.jackson@gmail.com>
2023-04-02 09:38:52 -0700

    Project Euler problem 8

--- project_euler.rb
+++ project_euler.rb
@@ -10,7 +10,7 @@ def problem_1(num)
 end
 
 prob1_num = 1_000
-puts "1. The sum of multiples of 3 or 5 below #{prob1_num} is: #{problem_1(prob1_num)}"
+puts "1. #{problem_1(prob1_num)}"
 
 # Problem 2 - Even Fibonacci numbers ##########################################
 #   Recursive solution, which I had to abandon because it causes a stack
@@ -41,7 +41,7 @@ def problem_2(num)
 end
 
 prob2_num = 4_000_000
-puts "2. The sum of even-valued Fibonacci terms below #{prob2_num} is: #{problem_2(prob2_num)}"
+puts "2. #{problem_2(prob2_num)}"
 
 # Problem 3 - Largest Prime Factor ############################################
 #   The prime factors of 13195 are 5, 7, 13 and 29.
@@ -73,7 +73,7 @@ def problem_3(num)
 end
 
 prob3_num = 600_851_475_143
-puts "3. The largest prime factor of the number #{prob3_num} is: #{problem_3(prob3_num)}"
+puts "3. #{problem_3(prob3_num)}"
 
 # Problem 4 - Largest Palindrome Factor #######################################
 #   A palindromic number reads the same both ways.
@@ -103,7 +103,7 @@ def is_palindrome?(num)
   return str == str.reverse
 end
 
-puts "4. The largest palindromic product of two 3-digit numbers is: #{problem_4}"
+puts "4. #{problem_4}"
 
 # Problem 5 - Smallest multiple ###############################################
 #   2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
@@ -123,7 +123,7 @@ def problem_5(lower_limit, upper_limit)
   return numerator
 end
 
-puts "5. The smallest number evenly divisible by 1 to 20 is: #{problem_5(2, 20)}"
+puts "5. #{problem_5(2, 20)}"
 
 # Problem 6 - Sum square difference ###############################################
 #   The sum of the squares of the first ten natural numbers is
@@ -145,7 +145,7 @@ def problem_6(num)
 end
 
 prob6_num = 100
-puts "6. The difference between the sum of the squares of the first #{prob6_num} natural numbers and the square of the sum is: #{problem_6(prob6_num)}"
+puts "6. #{problem_6(prob6_num)}"
 
 # Problem 7 - 10001st prime ###############################################
 #   By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
@@ -180,4 +180,48 @@ def problem_7(num)
 end
 
 prob7_num = 10_001
-puts "7. The #{prob7_num} prime number is: #{problem_7(prob7_num)}"
+puts "7. #{problem_7(prob7_num)}"
+
+# Problem 8 - Largest product in a series ###############################################
+#   The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+#     [1000-digit number omitted]
+#   Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+
+def problem_8(adjacent)
+  digits = <<~DIGITS
+    73167176531330624919225119674426574742355349194934
+    96983520312774506326239578318016984801869478851843
+    85861560789112949495459501737958331952853208805511
+    12540698747158523863050715693290963295227443043557
+    66896648950445244523161731856403098711121722383113
+    62229893423380308135336276614282806444486645238749
+    30358907296290491560440772390713810515859307960866
+    70172427121883998797908792274921901699720888093776
+    65727333001053367881220235421809751254540594752243
+    52584907711670556013604839586446706324415722155397
+    53697817977846174064955149290862569321978468622482
+    83972241375657056057490261407972968652414535100474
+    82166370484403199890008895243450658541227588666881
+    16427171479924442928230863465674813919123162824586
+    17866458359124566529476545682848912883142607690042
+    24219022671055626321111109370544217506941658960408
+    07198403850962455444362981230987879927244284909188
+    84580156166097919133875499200524063689912560717606
+    05886116467109405077541002256983155200055935729725
+    71636269561882670428252483600823257530420752963450
+  DIGITS
+  largest_product = 0
+  digits_array = digits.gsub(/\s+/, "").split("").map(&:to_i)
+
+  start_index = 0
+  while start_index < digits_array.length - adjacent
+    subarray = digits_array.slice(start_index, adjacent)
+    product = subarray.inject(:*)
+    largest_product = [product, largest_product].max
+    start_index += 1
+  end
+  largest_product
+end
+
+prob8_num = 13
+puts "8. #{problem_8(prob8_num)}"
